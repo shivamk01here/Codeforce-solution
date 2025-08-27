824b90c5140ee1c1457629a03641cd859c46ac38
shivam kumar <sk8612181@gmail.com>
2025-08-27 15:57:49 +0530

    feat: add word abbreviation program
    Implement a program that abbreviates long words by keeping the first and last letters with the count of removed letters in between. The solution handles input processing and outputs the abbreviated or original words based on length.

---
+++ A_WayTooLongWords/Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner; 
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);  
+        int n = sc.nextInt();
+        sc.nextLine(); 
+
+        for (int i = 0; i < n; i++) {
+            String word = sc.nextLine();
+            if (word.length() > 10) {
+                String abbreviated = word.charAt(0) + String.valueOf(word.length() - 2) + word.charAt(word.length() - 1);
+                System.out.println(abbreviated); 
+            } else {
+                System.out.println(word); 
+            }
+        }
+
+        sc.close();
+    }
+}
