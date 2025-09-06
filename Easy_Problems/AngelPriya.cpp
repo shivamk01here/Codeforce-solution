8aace809802d08ddcd1362b11586512217c3262b
shivam kumar <sk8612181@gmail.com>
2025-09-06 19:18:49 +0530

    feat: add solution to determine unique characters in name
    Implement a program that checks if the number of unique characters in a name is even or odd, printing "CHAT WITH HER!" for even and "IGNORE HIM!" for odd. This solves a basic string manipulation problem.

---
+++ Easy_Problems/AngelPriya.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+ 
+       string name ;
+       cin >> name;
+
+       string UniqueName = name.set();
+    
+       if(UniqueName.size() % 2 == 0){
+           cout << "CHAT WITH HER!" << endl;
+       }
+       else{
+           cout << "IGNORE HIM!" << endl;
+       }
+
+       // if(UniqueName.size() % 2 == 0)
+
+       
+}
