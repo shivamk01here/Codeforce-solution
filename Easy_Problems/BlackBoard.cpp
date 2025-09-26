49adc32e463abd311e36b8b2fb473f20a9a11feb
shivam kumar <sk8612181@gmail.com>
2025-09-26 17:28:21 +0530

    Indentation

--- Easy_Problems/BlackBoard.cpp
+++ Easy_Problems/BlackBoard.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <string>
-
 std::string solve() {
     int n;
     if (!(std::cin >> n)) return "";
@@ -10,16 +9,13 @@ std::string solve() {
         return "Alice";
     }
 }
-
 int main() {
     std::ios_base::sync_with_stdio(false);
     std::cin.tie(NULL);
-
     int t;
     if (!(std::cin >> t)) return 0;
     while (t--) {
         std::cout << solve() << "\n";
     }
-
     return 0;
 }
